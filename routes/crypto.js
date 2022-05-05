28cdbd8c179d8b2f620a7bac3457dad17d2830c7
Guangwei Na <gna@stevens.edu>
2022-05-05 00:27:30 -0400

    add new routes file

---
+++ routes/crypto.js
@@ -0,0 +1,8 @@
+const { coinspot } = require("ccxt")
+const req = require("express/lib/request")
+const router = require("./stocks")
+
+router.get crypto user all coinspot
+router.post req.date inputCryptoCode
+router.post('/tradecrypto')
+req.assetType
