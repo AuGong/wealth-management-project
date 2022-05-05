ec3a047c61c64ea1aad113635ca54e02bc94011c
Guangwei Na <gna@stevens.edu>
2022-05-04 23:29:42 -0400

    add crypto routes in route/index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./users');
 const stockRoutes = require('./stocks');
+const cryptoRoutes = require('./crypto');
 
 const constructorMethod = (app) => {
   app.use('/', userRoutes);
   app.use("/stocks", stockRoutes);
+  app.use('/crypto',cryptoRoutes);
   app.use("/crypto", (req, res) => {
     res.render("crypto", {});
   });
