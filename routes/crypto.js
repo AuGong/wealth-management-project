31112507393c57c3cb500b85ca525b60e5deaff3
Guangwei Na <gna@stevens.edu>
2022-05-05 17:45:42 -0400

    add route crypto

--- routes/crypto.js
+++ routes/crypto.js
@@ -1,8 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { ObjectId } = require('mongodb');
+const data = require('../data');
+const cryptoData = data.crpytos;
+
 const { coinspot } = require("ccxt")
 const req = require("express/lib/request")
-const router = require("./stocks")
 
 router.get crypto user all coinspot
 router.post req.date inputCryptoCode
 router.post('/tradecrypto')
 req.assetType
+
+router.get('/',async(req,res)=>{
+    try{
+        let res = cryptoData.searchCrypto()
+    }
+})
