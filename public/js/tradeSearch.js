923f5b467b236254022d85eeb27af969d1017ff0
mickylab <42708694+mickylab@users.noreply.github.com>
2022-05-05 22:40:09 -0400

    add change route

--- public/js/tradeSearch.js
+++ public/js/tradeSearch.js
@@ -1,20 +1,26 @@
 (function ($) {
 
-    let assetType = $("#inputAssetType").val().trim();
-
     
-
 
     $("#button-addon").click(function (event) {
         event.preventDefault();
+        let assetType = $("#inputAssetType option:selected").val().trim();
         let searchCode = $("#inputStockCode").val().trim();
         searchCode = searchCode.toUpperCase();
         if (searchCode) {
-            var searchConfig = {
-            method: "GET",
-            url: `https://financialmodelingprep.com/api/v3/quote/${searchCode}?apikey=4116b7eb972d010e408e5e350e723b1a`,
-            };
-
+            if (assetType === "Stock") {
+                var searchConfig = {
+                    method: "GET",
+                    url: `https://financialmodelingprep.com/api/v3/quote/${searchCode}?apikey=4116b7eb972d010e408e5e350e723b1a`,
+                };
+            } else {
+                var searchConfig = {
+                  method: "GET",
+                  url: `https://financialmodelingprep.com/api/v3/quote/${searchCode}USD?apikey=4116b7eb972d010e408e5e350e723b1a`,
+                };
+                $("#tradeForm").attr("action", "/crypto/tradecrypto");
+            }
+        
             $.ajax(searchConfig).then(function (responseMessage) {
                 var stockInfo = $(responseMessage);
                 $("#inputStockPrice").val(stockInfo[0].price);
