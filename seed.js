654f680bdd1838a2463f67667a389ad8c9221819
Guangwei Na <gna@stevens.edu>
2022-05-07 15:20:50 -0400

    modify seedfile

--- seed.js
+++ seed.js
@@ -1,9 +1,12 @@
 const data = require('./data/');
 const connection = require('./config/mongoConnection');
+const { ObjectId } = require('mongodb');
 const stocks = data.stocks;
 const users = data.users;
-const cryptocurrency = data.cryptocurrency;
+const cryptocurrency = data.crpytos;
 const transactions = data.transactions;
+const cryptoNames = ['BTC','ETH','BNB','SOL','XRP','LUNA','ADA','DOGE','AVAX','DOT']
+
 
 
 const main = async () => {
@@ -16,10 +19,11 @@ const main = async () => {
     const stock5 = await stocks.createStock("UA");
     const stock6 = await stocks.createStock("WBD");
     const stock7 = await stocks.createStock("GT");
-    
+    for(let i = 0;i<cryptoNames.length;i++){
+        await cryptocurrency.insertUser((new ObjectId()).toString(),'Crypto',cryptoNames[i],50,'Buy',234)
+    }
     await connection.closeConnection();
     console.log('Done seeding databse!');
-     
 }
 main().catch((error) => {
     console.log(error);
