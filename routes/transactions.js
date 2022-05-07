5d87e1030fd9ced4f8eb904f16304e0629923e0e
mickylab <42708694+mickylab@users.noreply.github.com>
2022-05-06 21:33:09 -0400

    update transaction

--- routes/transactions.js
+++ routes/transactions.js
@@ -27,7 +27,7 @@ router.get('/', async(req,res) =>{
     }
 });
 
-router.post('/searchBySymbol', async(req,res) =>{
+router.post('/', async(req,res) =>{
     if (req.session.user) {
         let userId = req.session.user._id;
         let symbol = req.body.inputStockCode;
